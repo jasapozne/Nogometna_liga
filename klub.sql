9d315e9ee92480d39edeb624146a351439454359
Jaša Pozne <jasa.pozne@gmail.com>
2022-05-05 12:49:00 +0200

    tabele

--- klub.sql
+++ klub.sql
@@ -1,47 +1,40 @@
-CREATE TABLE mostvo (
+CREATE TABLE osebe (
     emso INTEGER PRIMARY KEY,
     ime TEXT NOT NULL,
     priimek TEXT NOT NULL,
-    rojstvo DATE NOT NULL DEFAULT now(),
-    drzavljanstvo TEXT NOT NULL,
-    polozaj TEXT NOT NULL,
+    rojstvo DATE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE igralec (
+    pozicija TEXT NOT NULL,
     visina INTEGER NOT NULL,
-    teza INTEGER NOT NULL
-);
-
-CREATE TABLE pogodbe (
-    id INTEGER PRIMARY KEY,
-    datum_zacetka DATE NOT NULL DEFAULT now(),
-    datum_konca DATE NOT NULL DEFAULT now(),  
+    teza INTEGER NOT NULL,
     vrednost INTEGER NOT NULL,
+    datum_zacetka DATE NOT NULL DEFAULT now(),
+    datum_konca DATE NOT NULL DEFAULT now()
 );
 
-CREATE TABLE vodstvo (
-    emso INTEGER PRIMARY KEY,
-    ime TEXT NOT NULL,
-    priimek TEXT NOT NULL,  
-    polozaj TEXT NOT NULL,
+CREATE TABLE zaposlen (
+    delovno_mesto TEXT PRIMARY KEY,
+    placa INTEGER NOT NULL
 );
 
-CREATE TABLE statistika (
-    emso INTEGER PRIMARY KEY,
-    goli INTEGER NOT NULL,
-    podaje INTEGER NOT NULL,  
-    ocene INTEGER NOT NULL,
-);
-
-CREATE TABLE dresi (
-    id INTEGER PRIMARY KEY,
-    stevilo_dresa INTEGER NOT NULL,
-    cena INTEGER NOT NULL,  
-    prodani_dresi INTEGER NOT NULL
-);
-
-CREATE TABLE rezultati (
-    id_tekme INTEGER PRIMARY KEY,
-    izid INTEGER NOT NULL,
+CREATE TABLE ekipa (
+    ime TEXT PRIMARY KEY,
+    mesto TEXT NOT NULL,
+    stadion TEXT NOT NULL,
     domaca_ekipa TEXT NOT NULL,  
-    gostujoca_ekipa TEXT NOT NULL,
+    gostujoca_ekipa TEXT NOT NULL
+);
+
+CREATE TABLE tekma (
+    id_tekme INTEGER PRIMARY KEY,
+    izid INTEGER NOT NULL
+);
+
+CREATE TABLE goli (
+    strelec TEXT PRIMARY KEY,
+    podajalec TEXT NOT NULL
 );
 
 GRANT ALL ON DATABASE sem2022_jasap TO jasap;
