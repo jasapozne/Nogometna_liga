3ac6ae0fa90b837b1f73381ca3d1b23093d29c98
zigagartner <ziga.gartner@gmail.com>
2022-05-16 16:34:45 +0200

    Uvoz

---
+++ uvoz_podatkov.py
@@ -0,0 +1,24 @@
+import psycopg2, psycopg2.extensions, psycopg2.extras
+import csv
+import pandas as pd
+import os
+
+from auth_public import *
+
+
+
+#psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+#conn = psycopg2.connect(database=d, host=host, user=user, password=password)
+
+
+def uvozi_ekipa():
+    data = pd.read_csv("podatki/csv/ekipa.csv")
+    f = open("podatki/sql/ekipa.sql", "w")
+    for i in range(len(data.index)):
+        stolpci = "(" + ", ".join(str(v) for v in list(data.columns)) + ")"
+        vrednosti = "('" + "', '".join(str(v) for v in data.iloc[[i]].values.tolist()[0]) + "')"
+        f.write("INSERT INTO {tabela} {stolpci} VALUES {vrednosti} \n".format(tabela="ekipa", stolpci=stolpci, vrednosti=vrednosti))
+    f.close()
+
+
+         
