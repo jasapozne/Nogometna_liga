06b13e1a98d6780a3cd05cac9bdea71fb4ffa81b
Jaša Pozne <jasa.pozne@gmail.com>
2022-05-17 16:47:32 +0200

    Popravki

--- klub.sql
+++ klub.sql
@@ -2,7 +2,8 @@ CREATE TABLE osebe (
     emso INTEGER PRIMARY KEY,
     ime TEXT NOT NULL,
     priimek TEXT NOT NULL,
-    rojstvo DATE NOT NULL DEFAULT now()
+    rojstvo DATE NOT NULL DEFAULT now(),
+    ekipa TEXT NOT NULL
 );
 
 CREATE TABLE igralec (
@@ -11,30 +12,34 @@ CREATE TABLE igralec (
     teza INTEGER NOT NULL,
     vrednost INTEGER NOT NULL,
     datum_zacetka DATE NOT NULL DEFAULT now(),
-    datum_konca DATE NOT NULL DEFAULT now()
+    datum_konca DATE NOT NULL DEFAULT now(),
+    emso INTEGER NOT NULL REFERENCES osebe(emso) 
 );
 
 CREATE TABLE zaposlen (
-    delovno_mesto TEXT PRIMARY KEY,
+    emso INTEGER osebe(emso),
+    delovno_mesto TEXT NOT NULL,
     placa INTEGER NOT NULL
 );
 
 CREATE TABLE ekipa (
     ime TEXT PRIMARY KEY,
     mesto TEXT NOT NULL,
-    stadion TEXT NOT NULL,
-    domaca_ekipa TEXT NOT NULL,  
-    gostujoca_ekipa TEXT NOT NULL
-);
-
-CREATE TABLE tekma (
-    id_tekme INTEGER PRIMARY KEY,
-    izid INTEGER NOT NULL
+    stadion TEXT NOT NULL
 );
 
 CREATE TABLE goli (
-    strelec TEXT PRIMARY KEY,
-    podajalec TEXT NOT NULL
+    id_tekme INTEGER PRIMARY KEY REFERENCES tekma(id_tekme),
+    strelec INTEGER NOT NULL REFERENCES osebe(emso), 
+    podajalec INTEGER NOT NULL REFERENCES osebe(emso) 
+);
+
+CREATE TABLE tekma (
+    id_tekme INTEGER PRIMARY KEY
+    domaca_eikpa TEXT NOT NULL,
+    tuja_ekipa TEXT NOT NULL,
+    goli_domace INTEGER NOT NULL,
+    goli_tuje INTEGER NOT NULL
 );
 
 GRANT ALL ON DATABASE sem2022_jasap TO jasap;
