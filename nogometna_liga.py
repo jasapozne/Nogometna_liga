247eca830d34ab4cf1cb64cd6b60addb301ceca1
Jaša Pozne <jasa.pozne@gmail.com>
2022-08-08 23:12:08 +0200

    Popravil stare tabele in dodal dve novi

--- nogometna_liga.py
+++ nogometna_liga.py
@@ -24,17 +24,22 @@ def ekipa():
 
 @get('/goli')
 def goli():
-    cur.execute("""SELECT id_tekme,strelec,podajalec from goli""")
+    cur.execute("""SELECT tekma.domaca_ekipa, tekma.tuja_ekipa,oseba.ime,oseba.priimek, podaja.ime,podaja.priimek,oseba.ekipa from goli
+                    JOIN oseba ON oseba.emso = goli.strelec
+                    JOIN oseba AS podaja ON podaja.emso = goli.podajalec 
+                    JOIN tekma ON tekma.id_tekme = goli.id_tekme
+                    """)
     return template('goli.html', goli=cur)
 
 @get('/igralec')
 def igralec():
-    cur.execute("""SELECT pozicija,visina,teza,zacetek_pogodbe,konec_pogodbe,vrednost,emso from igralec""")
+    cur.execute("""SELECT oseba.ime, oseba.priimek, pozicija,visina,teza,zacetek_pogodbe,konec_pogodbe,vrednost, oseba.ekipa from igralec
+                    LEFT JOIN oseba ON oseba.emso = igralec.emso""")
     return template('igralec.html', igralec=cur)
 
 @get('/oseba')
 def oseba():
-    cur.execute("""SELECT emso,ime,priimek,rojstni_dan,ekipa from oseba""")
+    cur.execute("""SELECT ime,priimek,rojstni_dan,ekipa from oseba""")
     return template('oseba.html', oseba=cur)
 
 @get('/tekma')
@@ -44,8 +49,29 @@ def tekma():
 
 @get('/zaposlen')
 def zaposlen():
-    cur.execute("""SELECT delovno_mesto,placa,emso from zaposlen""")
+    cur.execute("""SELECT oseba.ime, oseba.priimek,delovno_mesto,placa,oseba.ekipa from zaposlen
+                    LEFT JOIN oseba ON oseba.emso = zaposlen.emso""")
     return template('zaposlen.html', zaposlen=cur)
+
+
+@get('/strelci')
+def strelci():
+    cur.execute("""SELECT oseba.ime,oseba.priimek, count(strelec),oseba.ekipa from goli
+                LEFT JOIN oseba ON oseba.emso = goli.strelec
+                GROUP BY oseba.ime,oseba.priimek,oseba.ekipa
+                ORDER BY count(strelec) DESC
+                LIMIT 20""")
+    return template('strelci.html', strelci=cur)
+
+@get('/podajalci')
+def podajalci():
+    cur.execute("""SELECT oseba.ime,oseba.priimek, count(podajalec),oseba.ekipa from goli
+                LEFT JOIN oseba ON oseba.emso = goli.podajalec
+                GROUP BY oseba.ime,oseba.priimek,oseba.ekipa
+                ORDER BY count(podajalec) DESC
+                LIMIT 20""")
+    return template('podajalci.html', podajalci=cur)
+
 
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
